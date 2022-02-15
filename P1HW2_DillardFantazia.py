9f194250f4e66257620b32f1d41fa0ab06970a8c
dillardf3314 <99154042+dillardf3314@users.noreply.github.com>
2022-02-14 19:08:04 -0500

    Add files via upload
    P1HW2

---
+++ P1HW2_DillardFantazia.py
@@ -0,0 +1,14 @@
+#Number of Pizzas to order
+#14-Feb-2022
+#CTI-110 P1HW2-Pizza Order
+#Fantazia Dillard
+#
+students = int(input('How many students do you want to order pizza for?'))
+total_slices = students *3
+total_pizza = students / 2
+print()
+print('----Pizza Order--------')
+print('Number of Students: ',students)
+print('Pizza Slices Needed: ',total_slices)
+print('Pizzas Needed :',total_pizza)
+print('--------------------------')
