3981c4bee2ff067af43f99758be3343401a9f52d
dillardf3314 <99154042+dillardf3314@users.noreply.github.com>
2022-03-28 18:34:41 -0400

    Add files via upload

---
+++ p4LAB2a_DillardFantazia.py
@@ -0,0 +1,23 @@
+import turtle
+
+triangle = turtle.Turtle()
+
+for t in range(3):
+    
+    triangle.forward(200)
+    triangle.left(120)
+
+
+
+square = turtle.Turtle()
+
+for i in range(4):
+    square.forward(100)
+    square.left(90)
+
+
+
+   
+
+
+
